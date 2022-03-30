b3f16d905c959482f2072aeb28d373d95814b6d6
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2022-03-30 09:41:14 +0200

    Added Images on welcome page and fixed z-index in nav and container

--- src/sections/welcome/Welcome.tsx
+++ src/sections/welcome/Welcome.tsx
@@ -1,10 +1,14 @@
 import { AiFillHeart } from 'react-icons/ai'
 import { Button } from '../../components/button/Button'
+import welcome1 from '../../images/Welcome1.svg'
+import welcome2 from '../../images/Welcome2.svg'
 
 import classes from './Welcome.module.scss'
 export const Welcome = () => {
 	return (
 		<section className={classes.welcome}>
+			<img src={welcome1} alt='' className={classes.welcome1} />
+			<img src={welcome2} alt='' className={classes.welcome2} />
 			<div className='container'>
 				<h1>A design agency with a twist</h1>
 				<h2>Design subscriptions to scale your business.</h2>
